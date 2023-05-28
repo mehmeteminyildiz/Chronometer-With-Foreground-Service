6e7208f054ceb980662ad988f3530aa63336aef1
Mehmet Yildiz <mhmtxyildiz@gmail.com>
2023-05-28 22:31:21 +0300

    Sticky Notification updating process completed

--- app/src/main/java/com/example/chronometerwithforegroundservice/service/TimerService.kt
+++ app/src/main/java/com/example/chronometerwithforegroundservice/service/TimerService.kt
@@ -1,21 +1,26 @@
 package com.example.chronometerwithforegroundservice.service
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.example.chronometerwithforegroundservice.MainActivity
 import com.example.chronometerwithforegroundservice.R
 import com.example.chronometerwithforegroundservice.model.TimerEvent
 import com.example.chronometerwithforegroundservice.util.Constants
 import com.example.chronometerwithforegroundservice.util.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.chronometerwithforegroundservice.util.Constants.NOTIFICATION_CHANNEL_NAME
+import com.example.chronometerwithforegroundservice.util.TimerUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -60,7 +65,25 @@ class TimerService : LifecycleService() {
         }
 
         startForeground(Constants.NOTIFICATION_ID, getNotificationBuilder().build())
+
+        timerInMillis.observe(this, Observer {
+            if (!isServiceStopped) {
+                val builder = getNotificationBuilder().setContentText(
+                    TimerUtil.getFormattedTime(it, false)
+                )
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return@Observer
+                }
+                notificationManager.notify(Constants.NOTIFICATION_ID, builder.build())
+            }
+        })
+
     }
+
 
     private fun initValues() {
         timerEvent.postValue(TimerEvent.END)
